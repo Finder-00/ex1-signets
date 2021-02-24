af4804f727f7427ea46aff89c549351d0fed542b
Finder-00 <you@example.com>
2021-02-24 18:16:51 -0500

    ajout icone liste, bouton bas fini et hover sur icone

--- src/Signet.jsx
+++ src/Signet.jsx
@@ -1,12 +1,13 @@
 import './Signet.scss';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SortOutlinedIcon from '@material-ui/icons/SortOutlined';
 
 export default function Signet(props){
 
     return(
         <div className= "LeSignet">
-            <img src={"imageSignets/"+props.id+".jpg"} alt="allo"/>
-            {/* <img src="imageSignets/001.jpg" alt=""/> */}
+            <img src={"imageSignets/"+props.id+".jpg"} alt="paysage"/>
+            <SortOutlinedIcon className="liste"/>
             <div className="contenu" style={{backgroundColor: props.couleur}}>
                 <div className="titre">{props.titre}</div>
                 <div className="date"> {props.dateModif} </div>
