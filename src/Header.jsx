40673ac3c47817e882287cb892e001a848dfab28
Finder-00 <you@example.com>
2021-02-24 18:51:07 -0500

    presque fini, manque juste le bug height de la page html...

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,10 @@ export default function Header() {
     return(
         <header>
             <div>Signet (Beta)</div>
-            <div>Francis desjardin</div>
+            <div className="user">
+                <div>Francis desjardin</div>
+                <img src="imageSignets/001.jpg" alt="paysage"/>
+            </div>
         </header>
     );
 }
